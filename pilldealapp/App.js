8f1af1917c616e7809cfa3e2f25e29b5eab23486
Joao Veloso <joao.goncalo.veloso@gmail.com>
2019-04-21 21:15:47 +0100

    a

--- pilldealapp/App.js
+++ pilldealapp/App.js
@@ -1,14 +1,150 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+//import React, { Component } from 'react';
+
+import { StyleSheet, Platform, Image, Text, View, ScrollView,
+  Button, FlatList, Switch } from 'react-native';
 
 import firebase from 'react-native-firebase';
+//http://nobrok.com/react-native-smart-switch-app-arduino-part-1/
+import BluetoothSerial from 'react-native-bluetooth-serial' //from bluetooth tutorial
+
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
+  constructor (props) {
+    super(props)
+    this.state = {
+      isEnabled: false,
+      discovering: false,
+      devices: [],
+      unpairedDevices: [],
+      connected: false,
+    }
   }
 
+
+  async omponentWillMount(){
+
+    //checks if BLT is on and witch devices are available
+    Promise.all([
+      BluetoothSerial.isEnabled(),
+      BluetoothSerial.list()
+    ])
+    .then((values) => {
+      const [ isEnabled, devices ] = values
+
+      this.setState({ isEnabled, devices })
+    })
+
+    //Initializes BLT, and save the new devices
+    BluetoothSerial.on('bluetoothEnabled', call_b => {
+
+      Promise.all([
+        BluetoothSerial.isEnabled(),
+        BluetoothSerial.list()
+      ])
+      .then((values) => {
+        const [ isEnabled, devices ] = values
+        this.setState({  devices })
+      })
+
+      BluetoothSerial.on('bluetoothDisabled', call_b2 => {
+
+         this.setState({ devices: [] })
+
+      })
+      BluetoothSerial.on('error', (err) => console.log(`Error: ${err.message}`))
+
+    })
+
+  }
+
+
+
+  _renderItem(item){
+
+    return(<View style={styles.deviceNameWrap}>
+            <Text style={styles.deviceName}>{item.item.name}</Text>
+          </View>)
+  }
+  enable () {
+    BluetoothSerial.enable()
+    .then((res) => this.setState({ isEnabled: true }))
+    .catch((err) => Toast.showShortBottom(err.message))
+  }
+
+  disable () {
+    BluetoothSerial.disable()
+    .then((res) => this.setState({ isEnabled: false }))
+    .catch((err) => Toast.showShortBottom(err.message))
+  }
+
+  toggleBluetooth (value) {
+    if (value === true) {
+      this.enable()
+    } else {
+      this.disable()
+    }
+  }
+  render() {
+
+    return (
+      <View style={styles.container}>
+      <View style={styles.toolbar}>
+
+            <Text style={styles.toolbarTitle}>Bluetooth Device List</Text>
+
+            <View style={styles.toolbarButton}>
+
+              <Switch
+                value={this.state.isEnabled}
+                onValueChange={(val) => this.toggleBluetooth(val)}
+              />
+
+            </View>
+      </View>
+        <FlatList
+          style={{flex:1}}
+          data={this.state.devices}
+          keyExtractor={item => item.id}
+          renderItem={(item) => this._renderItem(item)}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  toolbar:{
+    paddingTop:30,
+    paddingBottom:30,
+    flexDirection:'row'
+  },
+  toolbarButton:{
+    width: 50,
+    marginTop: 8,
+  },
+  toolbarTitle:{
+    textAlign:'center',
+    fontWeight:'bold',
+    fontSize: 20,
+    flex:1,
+    marginTop:6
+  },
+  deviceName: {
+    fontSize: 17,
+    color: "black"
+  },
+  deviceNameWrap: {
+    margin: 10,
+    borderBottomWidth:1
+  }
+});
+
+/*
   async componentDidMount() {
     // TODO: You: Do firebase things
     // const { user } = await firebase.auth().signInAnonymously();
@@ -16,6 +152,7 @@ export default class App extends React.Component {
 
     // await firebase.analytics().logEvent('foo', { bar: '123'});
   }
+
 
   render() {
     return (
@@ -100,3 +237,4 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   }
 });
+*/
